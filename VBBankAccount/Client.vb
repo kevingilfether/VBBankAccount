1f9424a0a4e1e0c3144a6a58ce64a67f33f88419
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-14 15:55:44 -0600

    Finished Client Class Draft

--- VBBankAccount/Client.vb
+++ VBBankAccount/Client.vb
@@ -6,9 +6,8 @@
     Private _acctName As String
     Private _userName As String
 
-
-
-    Public Property userID() As Int32
+    'Properties
+    Public Property UserID() As Int32
         Get
             Return _userID
         End Get
@@ -17,5 +16,38 @@
         End Set
     End Property
 
+    Public Property AcctName() As String
+        Get
+            Return _acctName
+        End Get
+        Set(value As String)
+            _acctName = value
+        End Set
+    End Property
+
+    Public Property UserName() As String
+        Get
+            Return _userName
+        End Get
+        Set(value As String)
+            _userName = value
+        End Set
+    End Property
+
+
+    'Constructors
+    Public Sub New()
+
+    End Sub
+
+    Public Sub New(ByVal _userID As Int32, ByVal _acctName As String, ByVal _userName As String)
+        Me._userID = _userID
+        Me._acctName = _acctName
+        Me._userName = _userName
+    End Sub
+
+    Public Sub ViewClientInfo()
+        Console.WriteLine(_userName + " --- " + _acctName + " --- " + _userID)
+    End Sub
 
 End Class
