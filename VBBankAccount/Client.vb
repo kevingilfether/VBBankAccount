5c3b339816a9e5d4087cef5390401fd761fbdf32
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-14 14:31:44 -0600

    Initialized Fields for Client Class

---
+++ VBBankAccount/Client.vb
@@ -0,0 +1,13 @@
+﻿Public Class Client
+    'Class for a customer at the Bank
+
+    'Fields
+    Private userID As Int32
+    Private acctName As String
+    Private userName As String
+
+
+
+    Property name
+
+End Class
