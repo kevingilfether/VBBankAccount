30c909eabf664760e3fd84afbadf5c7e73e4505b
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-14 16:14:42 -0600

    Starting abstract account class with overidable methods

---
+++ VBBankAccount/HelperClasses/Account.vb
@@ -0,0 +1,36 @@
+﻿MustInherit Class Account
+
+    Protected _acctID As Int32
+    Protected _balance As Double
+    Protected _acctType As String
+
+    Public ReadOnly Property AcctID() As Int32
+        Get
+            Return _acctID
+        End Get
+    End Property
+
+    Public ReadOnly Property Balance() As Double
+        Get
+            Return _balance
+        End Get
+    End Property
+
+    Public ReadOnly Property AcctType() As String
+        Get
+            Return _acctType
+        End Get
+    End Property
+
+    Public Sub New()
+
+    End Sub
+
+    Public Overridable Function Deposit(ByVal numDeposited As Double)
+        _balance += numDeposited
+        Return _balance
+    End Function
+
+
+
+End Class
